7b62ce54dfe1e8715e8701690e915e088fd769e5
guillecg <guillemcg92@gmail.com>
2023-03-14 11:44:13 +0100

    Refactor to get ECs from gene descriptions

--- modules/bioserv_kegg_genes.py
+++ modules/bioserv_kegg_genes.py
@@ -1,8 +1,8 @@
 import os
+import re
 import logging
 
-from io import StringIO
-
+import csv
 import pandas as pd
 
 from bioservices.kegg import KEGG
@@ -27,28 +27,112 @@ logger.info(f"Starting with organism {ORGANISM}")
 s = KEGG()
 s.organism = ORGANISM
 
-# KEGG retrieves all enzyme IDs although it filters pathway IDs by organism
+# KEGG retrieves all enzyme and modules IDs although it filters pathway IDs by
+# organism
 logger.info(f"Number of enzymes: {len(s.enzymeIds)}")
 logger.info(f"Number of pathways: {len(s.pathwayIds)}")
 
 
 # TODO: check https://www.genome.jp/dbget-bin/get_linkdb?-t+genes+gn:T04747
 
-# Get all genes for the given organism
-genes = s.list(ORGANISM)
 
-gene_cols = [
+results_columns = [
     "GENE_ID",
-    "GENE_TYPE",
-    "COORDINATES",
-    "DEFINITION"
+    "GENE_NAME",
+    "KO_NUMBER",
+    "EC_NUMBER",
+    "PATHWAY_ID",
+    "PATHWAY_NAME",
+    "KO_PATHWAY"
+    "ORGANISM"
 ]
-genes_df = pd.read_csv(
-    StringIO(genes),
-    header=None,
-    sep="\t",
-    names=gene_cols
+
+# Write empty CSV to later append to
+results_filepath = f"{FILE}_{ORGANISM}.csv"
+pd.DataFrame(columns=results_columns).to_csv(
+    results_filepath,
+    header=True,
+    index=False,
+    sep=",",
+    mode="w"
 )
 
-# TODO: not working - find ECs
-genes_df[genes_df["DESCRIPTION"].str.contains("\[EC\:")]
+# NOTE: In order to retrieve information from RetroRules, we need either the
+# reaction numbers or the ECs. The bioservices package retrieves, among other
+# information, modules for the given pathway in case they are available.
+# Modules are important because they contain reaction numbers. However, some 
+# pathways may lack any modules and, thus, the information regarding the 
+# reactions. Genes (EC numbers) information is not present either.
+
+for pathway in s.pathwayIds:
+
+    logger.info(f"Starting with pathway {pathway}")
+
+    # Retrieve info for the given pathway
+    pathway_res = s.get(pathway)
+    pathway_res = s.parse(pathway_res)
+
+    pathway_df = pd.DataFrame(columns=results_columns)
+
+    # There may be errors due to pathways lacking any modules
+    # TODO: try extracting EC from ko_res["MODULE"]
+    if "GENE" not in pathway_res.keys():
+        logger.warning(f"No genes found for {pathway}")
+        continue
+
+    # Retrieve info for all the modules in the KO pathway
+    for gene_id, gene_desc in pathway_res["GENE"].items():
+
+        logger.info(f"Processing gene {gene_id}")
+
+        # TODO: get information for each gene
+        # gene_res = s.get(f"{ORGANISM}:{gene_id}")
+        # gene_res = s.parse(gene_res)
+
+        gene_desc = gene_desc.split(" [")
+        gene_desc = [x.replace("]", "") for x in gene_desc]
+
+        # Get KO and EC numbers by regex
+        gene_ko = list(
+            filter(lambda x: re.match(pattern="KO\:", string=x), gene_desc)
+        )
+        gene_ec = list(
+            filter(lambda x: re.match(pattern="EC\:", string=x), gene_desc)
+        )
+
+        # Remove prefix
+        gene_ko = [x.replace("KO:", "") for x in gene_ko]
+        gene_ec = [x.replace("EC:", "") for x in gene_ec]
+
+        # Join multiple numbers
+        gene_ko = ";".join(gene_ko).replace(" ", ";")
+        gene_ec = ";".join(gene_ec).replace(" ", ";")
+
+        # Concatenate results to final dataframe
+        row = pd.Series({
+            "GENE_ID": gene_id,
+            "GENE_NAME": gene_desc[0],
+            "KO_NUMBER": gene_ko,
+            "EC_NUMBER": gene_ec,
+            "PATHWAY_ID": pathway,
+            "PATHWAY_NAME": pathway_res["NAME"][0],
+            "KO_PATHWAY": pathway_res["KO_PATHWAY"],
+            "ORGANISM": pathway_res["ORGANISM"]
+        })
+        pathway_df = pd.concat(
+            [pathway_df, row.to_frame().T],
+            axis=0,
+            ignore_index=True
+        )
+
+    logger.info(f"Writing pathway {pathway} to file")
+
+    pathway_df.to_csv(
+        results_filepath,
+        header=False,
+        index=False,
+        sep=",",
+        mode="a",
+        quotechar='"',
+        quoting=csv.QUOTE_MINIMAL # Avoid errors with commas in names
+    )
