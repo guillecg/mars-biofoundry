c14b254d6907d42028dfd8e7f2b9c7db3e8c7106
guillecg <guillemcg92@gmail.com>
2023-04-19 18:44:13 +0200

    Fix retrieval of substrates for the sink

--- modules/get_sink.py
+++ modules/get_sink.py
@@ -55,18 +55,57 @@ rules_df = pd.read_csv(
 # Get all MetaNetX reaction IDs
 rules_df[["MNXR", "MNXM"]]  = rules_df["Rule ID"].str.split("_", expand=True)
 
-# Get all reaction information by MNXR
-reactions_df = metanetx_reac_prop[
-    metanetx_reac_prop["ID"].isin(rules_df["MNXR"].unique())
-]
+# Drop diameter and rule usage duplicates
+unique_rules_df = rules_df.drop_duplicates(subset=["MNXR", "Rule usage"])
 
-# Extract compound IDs from mnx_equation
-compounds_df = reactions_df["mnx_equation"]\
+# Get all reaction information by MNXR
+reactions_df = pd.merge(
+    left=metanetx_reac_prop,
+    right=unique_rules_df,
+    left_on="ID",
+    right_on="MNXR",
+    how="outer",
+    indicator=True
+)
+
+# Remove "left_only" reactions (not present in the organism rules)
+reactions_df = reactions_df[reactions_df["_merge"] != "left_only"]
+
+# Get all reactions that could not be mapped in METANETX
+not_in_metanetx = reactions_df[reactions_df["_merge"] == "right_only"]
+print(
+    "[WARNING] #reactions not present in METANETX: ",
+    len(not_in_metanetx), "/", len(reactions_df)
+)
+
+# Finally, get reactions in the organism that could be mapped to METANETX
+reactions_df = reactions_df[reactions_df["_merge"] == "both"]
+
+# NOTE: METANETX reactions are in forward format. Therefore, we should get the
+# substrates for the sink.
+compounds_forward = reactions_df["mnx_equation"]\
+    .str.split("=").str[0]\
     .str.extractall(r"(MNXM\d+)")\
     .reset_index(drop=True)\
     .rename(columns={0: "Name"})
 
-# Drop duplicates
+# Add products of reversible reactions
+compounds_reverse = reactions_df[
+        reactions_df["Rule usage"] == "both"
+    ]["mnx_equation"]\
+    .str.split("=").str[-1]\
+    .str.extractall(r"(MNXM\d+)")\
+    .reset_index(drop=True)\
+    .rename(columns={0: "Name"})
+
+# Merge substrates and products (reversible reactions)
+compounds_df = pd.concat(
+    [compounds_forward, compounds_reverse],
+    axis=0,
+    ignore_index=True
+)
+
+# Drop potential duplicates (compounds appearing in more than one reation)
 compounds_df = compounds_df.drop_duplicates()
 
 # Get InChIs for the extracted compound IDs
@@ -78,7 +117,12 @@ compounds_df = pd.merge(
 )
 compounds_df = compounds_df[["Name", "InChI"]]
 
-compounds_df[compounds_df["InChI"].isnull()]
+# WARNING: there are compounds without InChI
+no_inchi = compounds_df[compounds_df["InChI"].isnull()]
+print(
+    "[WARNING] #compounds without InChI: ",
+    len(no_inchi), "/", len(compounds_df)
+)
 
 # Fill missing InChIs and match sink.csv format
 compounds_df = compounds_df.fillna("None")
