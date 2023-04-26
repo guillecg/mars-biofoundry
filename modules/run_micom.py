d9c96f3ef98e76b63d9318de03ab629d9293b0ff
guillecg <guillemcg92@gmail.com>
2023-04-26 08:44:21 +0200

    Fix model path

--- modules/run_micom.py
+++ modules/run_micom.py
@@ -5,7 +5,7 @@ import pandas as pd
 from micom import Community
 
 
-DATA_DIR = "../data/micom/"
+DATA_DIR = "../data/genomes/"
 
 # WARNING: Not working for more than one thread, caution is advised!
 # N_THREADS = 1
@@ -18,7 +18,8 @@ taxonomy = [
         "species": "Tessaracoccus sp. T2.5-30",
         "reactions": 305,
         "metabolites": 236,
-        "file": os.path.join(DATA_DIR, "tez.json")
+        "file": os.path.join(
+            DATA_DIR, "tez/tez.json")
     }
 ]
 taxonomy = pd.DataFrame.from_records(taxonomy * 4)
