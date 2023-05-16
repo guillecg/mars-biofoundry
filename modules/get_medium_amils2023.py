b6a86edc7c4c5878a310fa1c085c9df2b5ac224d
guillecg <guillemcg92@gmail.com>
2023-05-16 11:18:37 +0200

    Add dataset S3 and tables S1, S7 and S8-2

--- modules/get_medium_amils2023.py
+++ modules/get_medium_amils2023.py
@@ -1,3 +1,4 @@
+import os
 
 import pandas as pd
 
@@ -6,8 +7,21 @@ import plotly.express as px
 from tabula import read_pdf
 
 
+# NOTE: Most isolates are present at depth 487 (see Table S2 in
+# emi16291-sup-0001-supinfo.docx for Amils et al. 2023)
+
+
+PAPER_DIR = "../data/amils2023/"
+
+
+# ---------------------------------------------------------------------------- #
+# Dataset S2 - ICP-MS elemental analysis of core samples (ppm)
+
 elements_df = read_pdf(
-    input_path="../data/amils2023/emi16291-sup-0003-datasets2.pdf",
+    input_path=os.path.join(
+        PAPER_DIR,
+        "emi16291-sup-0003-datasets2.pdf"
+    ),
     pages=1
 )
 
@@ -18,8 +32,11 @@ elements_df = elements_df[0]
 elements_df_long = pd.melt(
     elements_df,
     id_vars=["Depth"],
-    value_vars=[col for col in elements_df.columns if col != "Depth"],
-    var_name="Element",
+    value_vars=[
+        col for col in elements_df.columns
+        if col not in ["Depth"]
+    ],
+    var_name="Species",
     value_name="Concentration (ppm)"
 )
 
@@ -29,9 +46,12 @@ elements_df_long[["Depth", "Concentration (ppm)"]] = \
     .apply(lambda row: row.str.replace(",", "."))\
     .astype(float)
 
+# Round to fit Illumina and Roche datasets (Datasets S4 and S5)
+elements_df_long["Depth"] = elements_df_long["Depth"].astype(int)
+
 # Get sorted elements by their maximum concentration
 elements_sorted = elements_df_long\
-    .groupby("Element")\
+    .groupby("Species")\
     .max()\
     .sort_values("Concentration (ppm)", ascending=False)\
     .index\
@@ -41,9 +61,9 @@ fig = px.scatter(
     data_frame=elements_df_long,
     x="Concentration (ppm)",
     y="Depth",
-    color="Element",
+    color="Species",
     color_discrete_sequence=px.colors.qualitative.Alphabet,
-    category_orders={"Element": elements_sorted},
+    category_orders={"Species": elements_sorted},
     title="Concentration of elements across the vertical column"
 )
 fig['layout']['yaxis']['autorange'] = "reversed"
@@ -54,9 +74,9 @@ fig = px.scatter(
     x="Concentration (ppm)",
     log_x=True,
     y="Depth",
-    color="Element",
+    color="Species",
     color_discrete_sequence=px.colors.qualitative.Alphabet,
-    category_orders={"Element": elements_sorted},
+    category_orders={"Species": elements_sorted},
     title="Concentration of elements across the vertical column"
 )
 fig.update_layout(xaxis_title="Concentration log(ppm)")
@@ -67,13 +87,228 @@ fig = px.violin(
     data_frame=elements_df_long,
     y="Concentration (ppm)",
     log_y=True,
-    color="Element",
+    color="Species",
     color_discrete_sequence=px.colors.qualitative.Alphabet,
-    category_orders={"Element": elements_sorted},
+    category_orders={"Species": elements_sorted},
     title="Distribution of concentrations per element"
 )
 fig.update_layout(
-    xaxis_title="Element",
+    xaxis_title="Species",
     yaxis_title="Concentration log(ppm)"
 )
 fig.show()
+
+
+# ---------------------------------------------------------------------------- #
+# Dataset S3 - Ionic chromatography of BH10 soluble organic and iniorganic anions (ppm)
+
+compounds_df = pd.read_excel(
+    os.path.join(
+        PAPER_DIR,
+        "emi16291-sup-0004-datasets3.xls"
+    ),
+    sheet_name="BH10_CI",
+    skiprows=1
+)
+
+# Create depth column
+compounds_df["Depth"] = compounds_df["SAMPLE"]\
+    .str.split("_").str[-1]\
+    .str.split("-").str[-1]
+
+compounds_df = compounds_df.drop("SAMPLE", axis=1)
+
+# Remove letters from numbers (e.g. depth W90)
+compounds_df["Depth"] = compounds_df["Depth"]\
+    .str.strip(r"\W")
+
+# Convert to numeric
+compounds_df["Depth"] = compounds_df["Depth"]\
+    .str.replace(",", ".")\
+    .astype(float)
+
+# Round to fit Illumina and Roche datasets (Datasets S4 and S5)
+compounds_df["Depth"] = compounds_df["Depth"].astype(int)
+
+compounds_df_long = pd.melt(
+    compounds_df,
+    id_vars=["Depth", "pH"],
+    value_vars=[
+        col for col in compounds_df.columns
+        if col not in ["Depth", "pH"]
+    ],
+    var_name="Species",
+    value_name="Concentration (ppm)"
+)
+
+# Capitalize to fit format
+compounds_df_long["Species"] = compounds_df_long["Species"].str.capitalize()
+
+
+# ---------------------------------------------------------------------------- #
+# Table S1 - Soluble cations (ppm)
+
+cations_df = pd.read_excel(
+    os.path.join(
+        PAPER_DIR,
+        "emi16291-sup-0001-supinfo-tables1.ods"
+    ),
+    sheet_name="Sheet1",
+    skiprows=1
+)
+
+# Round to fit Illumina and Roche datasets (Datasets S4 and S5)
+cations_df["Depth"] = cations_df["Depth"].astype(int)
+
+cations_df_long = pd.melt(
+    cations_df,
+    id_vars=["Depth"],
+    value_vars=[
+        col for col in cations_df.columns
+        if col not in ["Depth"]
+    ],
+    var_name="Species",
+    value_name="Concentration (ppm)"
+)
+
+
+# ---------------------------------------------------------------------------- #
+# Table S7 - Occluded gases and natural activities at different depths (10/8/22)
+
+gases_df = pd.read_excel(
+    os.path.join(
+        PAPER_DIR,
+        "emi16291-sup-0001-supinfo-tables7.ods"
+    ),
+    sheet_name="Sheet1",
+    skiprows=1
+)
+
+# Drop last row containing the explanation
+gases_df = gases_df.iloc[:-1, :].copy()
+
+# Drop last three columns since they correspond to activate metabolism
+gases_df = gases_df.iloc[:, :-3].copy()
+
+# Rename depth column
+gases_df = gases_df.rename(columns={"Depth mbs": "Depth"})
+
+# Round to fit Illumina and Roche datasets (Datasets S4 and S5)
+gases_df["Depth"] = gases_df["Depth"].astype(int)
+
+# Apply conversion of values
+h2_co2_symbol_map = {
+    "+++": 2000,
+    "++": 1000,
+    "+": 250,
+    "+/-": 40,
+    "-": 0
+}
+ch4_symbol_map = {
+    "+++": 35,
+    "++": 20,
+    "+": 10,
+    "+/-": 5,
+    "-": 0
+}
+
+gases_df["H2"] = gases_df["H2"].replace(h2_co2_symbol_map).astype(float)
+gases_df["CO2"] = gases_df["CO2"].replace(h2_co2_symbol_map).astype(float)
+gases_df["CH4"] = gases_df["CH4"].replace(ch4_symbol_map).astype(float)
+
+gases_df_long = pd.melt(
+    gases_df,
+    id_vars=["Depth"],
+    value_vars=[
+        col for col in gases_df.columns
+        if col not in ["Depth"]
+    ],
+    var_name="Species",
+    value_name="Concentration (ppm)"
+)
+
+# ---------------------------------------------------------------------------- #
+# Table S8-2 - Number of microbial species detected at different depths which
+# have the potential of carry out key metabolic pathways of the C, H, N, S and
+# Fe cycles. 
+
+microbes_df = pd.read_excel(
+    os.path.join(
+        PAPER_DIR,
+        "emi16291-sup-0001-supinfo-tables8-2.ods"
+    ),
+    sheet_name="Sheet1"
+)
+
+# Rename pathway column
+microbes_df = microbes_df.rename(columns={"Pathway/depth": "Pathway"})
+
+# Drop last row containing the explanation
+microbes_df = microbes_df.iloc[:-1, :].copy()
+
+# Drop rows containing the cycles
+microbes_df = microbes_df[
+    ~microbes_df["Pathway"].str.endswith(" cycle")
+]
+
+# Convert to numeric
+numeric_cols = [
+    col for col in microbes_df.columns
+    if col not in ["Pathway"]
+]
+microbes_df[numeric_cols] = microbes_df[numeric_cols].apply(
+    pd.to_numeric,
+    errors="coerce"
+)
+
+fig = px.imshow(
+    img=microbes_df[numeric_cols].T.to_numpy(),
+    x=microbes_df["Pathway"],
+    y=numeric_cols,
+    labels=dict(
+        x="Pathway",
+        y="Depth",
+        color="Count"
+    ),
+    aspect="equal",
+    width=600,
+    height=600,
+    title="Distribution of microbial functions across the vertical column"
+)
+fig.show()
+
+microbes_df_long = pd.melt(
+    microbes_df,
+    id_vars=["Pathway"],
+    value_vars=[
+        col for col in microbes_df.columns
+        if col not in ["Pathway"]
+    ],
+    var_name="Depth",
+    value_name="Count"
+)
+
+fig = px.bar(
+    data_frame=microbes_df_long,
+    x="Pathway",
+    y="Depth",
+    color="Count"
+)
+fig['layout']['yaxis']['autorange'] = "reversed"
+fig.show()
+
+
+# ---------------------------------------------------------------------------- #
+# Taxonomy abundances
+
+taxonomy_abundances = pd.read_csv(
+    os.path.join(
+        "../data/micom/rio_tinto/amils_2023/",
+        "taxonomy.csv"
+    ),
+    sep=","
+)
+
+# Create depth column
+taxonomy_abundances["Depth"] = taxonomy_abundances["sample_id"]\
+    .str.split("-").str[1]
