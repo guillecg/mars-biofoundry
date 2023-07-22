67ea33d780373ee7690685f88042828caf6b5ddc
guillecg <guillemcg92@gmail.com>
2023-07-22 20:56:47 +0200

    Refactor get_sink.py

--- modules/retropath/utils.py
+++ modules/retropath/utils.py
@@ -195,8 +195,167 @@ class RetroPathPreloader(object):
 
         return rules_df
 
-    def get_sink(self) -> None:
-        raise NotImplementedError
+    def get_sink(self) -> pd.DataFrame:
+        """
+        Get the sink by extracting compounds from the rules when:
+            - They appear in the reactants
+            - They appear in the products if their rule is reversible
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        sink_df : DataFrame
+            Dataframe containing as the compounds found for the given rules.
+
+        Examples
+        --------
+        None
+
+        """
+
+        # Read MetaNetX reac_prop.tsv file
+        metanetx_reac_prop = pd.read_table(
+            os.path.join(
+                self.config["paths"]["metanetx"],
+                "reac_prop.tsv"
+            ),
+            comment="#", # Skip comment rows
+            header=None,
+            names=[
+                "ID",
+                "mnx_equation",
+                "reference",
+                "classifs",
+                "is_balanced",
+                "is_transport"
+            ]
+        )
+
+        # Read MetaNetX chem_prop.tsv file
+        metanetx_chem_prop = pd.read_table(
+            os.path.join(
+                self.config["paths"]["metanetx"],
+                "chem_prop.tsv"
+            ),
+            comment="#", # Skip comment rows
+            header=None,
+            names=[
+                "Name", # Format for sink.csv
+                "compound",
+                "reference",
+                "formula",
+                "charge",
+                "mass",
+                "InChI",
+                "InChIKey",
+                "SMILES"
+            ]
+        )
+
+        # Load rules extracted by mapping ECs to RetroRules DB
+        rules_df = pd.read_csv(
+            os.path.join(
+                self.config["paths"]["retropath"],
+                self.config["retropath"]["files"]["rules"]
+            ),
+            sep=","
+        )
+
+        # Get all MetaNetX reaction IDs
+        rules_df[["MNXR", "MNXM"]]  = rules_df["Rule ID"]\
+            .str.split("_", expand=True)
+
+        # Drop diameter and rule usage duplicates
+        unique_rules_df = rules_df\
+            .drop_duplicates(subset=["MNXR", "Rule usage"])
+
+        # Get all reaction information by MNXR
+        reactions_df = pd.merge(
+            left=metanetx_reac_prop,
+            right=unique_rules_df,
+            left_on="ID",
+            right_on="MNXR",
+            how="outer",
+            indicator=True
+        )
+
+        # Remove "left_only" reactions (not present in the organism rules)
+        reactions_df = reactions_df[reactions_df["_merge"] != "left_only"]
+
+        # Get all reactions that could not be mapped in METANETX
+        not_in_metanetx = reactions_df[reactions_df["_merge"] == "right_only"]
+        print(
+            "[WARNING] #reactions not present in METANETX: ",
+            len(not_in_metanetx), "/", len(reactions_df)
+        )
+
+        # Finally, get reactions that could be mapped to METANETX
+        reactions_df = reactions_df[reactions_df["_merge"] == "both"]
+
+        # NOTE: METANETX reactions are in forward format. Therefore, we should 
+        # get the substrates for the sink.
+        compounds_forward = reactions_df["mnx_equation"]\
+            .str.split("=").str[0]\
+            .str.extractall(r"(MNXM\d+)")\
+            .reset_index(drop=True)\
+            .rename(columns={0: "Name"})
+
+        # Add products of reversible reactions
+        compounds_reverse = reactions_df[
+                reactions_df["Rule usage"] == "both"
+            ]["mnx_equation"]\
+            .str.split("=").str[-1]\
+            .str.extractall(r"(MNXM\d+)")\
+            .reset_index(drop=True)\
+            .rename(columns={0: "Name"})
+
+        # Merge substrates and products (reversible reactions)
+        sink_df = pd.concat(
+            [compounds_forward, compounds_reverse],
+            axis=0,
+            ignore_index=True
+        )
+
+        # Drop potential duplicates (compounds in more than one reation)
+        sink_df = sink_df.drop_duplicates()
+
+        # Get InChIs for the extracted compound IDs
+        sink_df = pd.merge(
+            left=sink_df,
+            right=metanetx_chem_prop,
+            on="Name",
+            how="left"
+        )
+        sink_df = sink_df[["Name", "InChI"]]
+
+        # WARNING: there are compounds without InChI
+        no_inchi = sink_df[sink_df["InChI"].isnull()]
+        print(
+            "[WARNING] #compounds without InChI: ",
+            len(no_inchi), "/", len(sink_df)
+        )
+
+        # Fill missing InChIs and match sink.csv format
+        sink_df = sink_df.fillna("None")
+
+        # Save to file
+        sink_df.to_csv(
+            os.path.join(
+                self.config["paths"]["retropath"],
+                self.config["retropath"]["files"]["sink"]
+            ),
+            header=True,
+            index=False,
+            sep=",",
+            mode="w",
+            quotechar='"',
+            quoting=csv.QUOTE_ALL # Fit sink.csv format for RetroPath2.0
+        )
+
+        return sink_df
 
     def get_sources(self) -> None:
         raise NotImplementedError
