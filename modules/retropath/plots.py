aa2787aaee4711948146a8985a2edfb11fbaa53c
guillecg <guillemcg92@gmail.com>
2023-07-22 22:16:28 +0200

    Refactor RetroPath plots

--- modules/retropath/plots.py
+++ modules/retropath/plots.py
@@ -2,98 +2,149 @@ import os
 
 import pandas as pd
 
-from retropath2_wrapper import retropath2
+import plotly
+import plotly.io as pio
+import plotly.express as px
 
 
 DATA_DIR = "../data/retropath/interesting_metabolites/"
 
 
-results_df = pd.DataFrame()
+def get_retropath_results(config: dict) -> pd.DataFrame:
+    """
+    Get the results of the RetroPath2.0 analysis by inspecting each source
+    folder.
 
-for source in os.listdir(os.path.join(DATA_DIR, "experiments")):
+    Parameters
+    ----------
+    config : dict
+        The configuration dictionary.
 
-    if source.endswith(".csv"):
-        continue
+    Returns
+    -------
+    results_df : DataFrame
+        Dataframe containing the results for each source.
 
-    source_files = os.listdir(os.path.join(DATA_DIR, "experiments", source))
+    Examples
+    --------
+    None
 
-    source_df = pd.DataFrame({
-        "Source": [source],
-        "Status": [None]
-    })
+    """
 
-    if "_scope.csv" in "".join(source_files):
-        source_df["Status"] = "Scope"
+    results_df = pd.DataFrame()
 
-    elif sorted(source_files) == sorted(["results.csv", "source-in-sink.csv"]):
-        source_df["Status"] = "Results but no scope"
+    experiments_dir = os.path.join(
+        config["paths"]["retropath"],
+        "interesting_metabolites/",
+        "experiments/"
+    )
 
-    elif source_files == ["source-in-sink.csv"]:
-        source_sink_df = pd.read_csv(
-            os.path.join(DATA_DIR, "experiments", source, "source-in-sink.csv")
+    for source in os.listdir(experiments_dir):
+
+        # Get all files for the given source
+        source_files = os.listdir(os.path.join(experiments_dir, source))
+
+        source_df = pd.DataFrame({
+            "Source": [source],
+            "Status": [None]
+        })
+
+        if "_scope.csv" in "".join(source_files):
+            source_df["Status"] = "Scope"
+
+        elif sorted(source_files) == sorted(["results.csv", "source-in-sink.csv"]):
+            source_df["Status"] = "Results but no scope"
+
+        elif source_files == ["source-in-sink.csv"]:
+            source_sink_df = pd.read_csv(
+                os.path.join(DATA_DIR, "experiments", source, "source-in-sink.csv")
+            )
+
+            if len(source_sink_df):
+                source_df["Status"] = "Source in sink"
+            else:
+                source_df["Status"] = "Source in sink (empty)"
+
+        else:
+            source_df["Status"] = "Error"
+
+        results_df = pd.concat(
+            [results_df, source_df],
+            axis=0,
+            ignore_index=True
         )
 
-        if len(source_sink_df):
-            source_df["Status"] = "Source in sink"
-        else:
-            source_df["Status"] = "Source in sink (empty)"
+    # Get metabolites not produced
+    all_sources = os.listdir(os.path.join(DATA_DIR, "sources"))
+    all_sources = set([item.replace(".csv", "") for item in all_sources])
 
-    else:
-        source_df["Status"] = "Error"
+    processed_sources = set(results_df["Source"].values)
+    not_processed_sources = all_sources - processed_sources
+
+    # TODO: log len(not_processed_sources), not_processed_sources
+
+    not_produced_df = pd.DataFrame({
+        "Source": list(not_processed_sources),
+        "Status": "Error"
+    })
 
     results_df = pd.concat(
-        [results_df, source_df],
+        [results_df, not_produced_df],
         axis=0,
         ignore_index=True
     )
 
-# Get not produced metabolites
-all_sources = os.listdir(os.path.join(DATA_DIR, "sources"))
-all_sources = set([item.replace(".csv", "") for item in all_sources])
-processed_sources = set(results_df["Source"].values)
-not_processed_sources = all_sources - processed_sources
-len(not_processed_sources), not_processed_sources
-
-not_produced_df = pd.DataFrame({
-    "Source": list(not_processed_sources),
-    "Status": "Error"
-})
-
-results_df = pd.concat(
-    [results_df, not_produced_df],
-    axis=0,
-    ignore_index=True
-)
-
-results_df.to_csv(
-    os.path.join(DATA_DIR, "experiments", "interesting_metabolites.csv"),
-    index=False,
-    header=True
-)
+    return results_df
 
 
-import plotly.io as pio
-import plotly.express as px
+def plot_retropath_results(
+    results_df: pd.DataFrame,
+    config: dict
+) -> plotly.graph_objs._figure.Figure:
+    """
+    Plot the results of the RetroPath2.0 analysis.
 
-pio.templates.default = "plotly_white"
+    Parameters
+    ----------
+    results_df : DataFrame
+        Dataframe containing the results for each source.
+    config : dict
+        The configuration dictionary.
 
-status_counts = results_df["Status"]\
-    .value_counts()\
-    .reset_index(drop=False)\
-    .rename(columns={"Status": "Counts", "index": "Status"})
+    Returns
+    -------
+    fig : Figure
+        The generated figure.
 
-fig = px.bar(
-    data_frame=status_counts,
-    x="Status",
-    y="Counts",
-    color="Status",
-    color_discrete_sequence=px.colors.qualitative.Pastel,
-    text_auto=True
-)
-fig.update_layout(showlegend=False)
+    Examples
+    --------
+    None
 
-pio.write_image(
-    fig=fig,
-    file="../data/figures/retropath-metabolites.jpg",
-    scale=6
-)
+    """
+
+    status_counts = results_df["Status"]\
+        .value_counts()\
+        .reset_index(drop=False)\
+        .rename(columns={"Status": "Counts", "index": "Status"})
+
+    fig = px.bar(
+        data_frame=status_counts,
+        x="Status",
+        y="Counts",
+        color="Status",
+        color_discrete_sequence=px.colors.qualitative.Pastel,
+        text_auto=True,
+        template=config["figures"]["template"]
+    )
+    fig.update_layout(showlegend=False)
+
+    pio.write_image(
+        fig=fig,
+        file=os.path.join(
+            config["paths"]["figures"],
+            "retropath-metabolites.jpg"
+        ),
+        scale=6
+    )
+
+    return fig
