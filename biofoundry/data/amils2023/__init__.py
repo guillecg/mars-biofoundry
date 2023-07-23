bb875006ff4ec7dfaea3390aba6b2f43e4ab1e36
guillecg <guillemcg92@gmail.com>
2023-07-23 19:56:58 +0200

    Move get_microbial_data to Amils2023DataLoader

--- biofoundry/data/amils2023/__init__.py
+++ biofoundry/data/amils2023/__init__.py
@@ -1,10 +1,9 @@
 from .data_loader import Amils2023DataLoader
-from .plots import plot_concentrations, get_microbial_data, plot_microbial_data
+from .plots import plot_concentrations, plot_microbial_data
 
 
 __all__ = [
     "Amils2023DataLoader",
     "plot_concentrations",
-    "get_microbial_data",
     "plot_microbial_data"
 ]
--- biofoundry/data/amils2023/plots.py
+++ biofoundry/data/amils2023/plots.py
@@ -83,60 +83,6 @@ def plot_concentrations(
     fig.show("png")
 
 
-def get_microbial_data(data_dir: str) -> pd.DataFrame:
-    """
-    Get the microbial data from table S8 in Amils et al. 2023.
-
-    Parameters
-    ----------
-    data_dir : str
-        The data directory containing emi16291-sup-0001-supinfo-tables8-2.ods.
-
-    Returns
-    -------
-    microbes_df : pandas.DataFrame
-        Dataframe containing the microbial data from Amils et al. 2023.
-
-    Examples
-    --------
-    >>> from biofoundry.data.amils2023 import Amils2023DataLoader
-    >>> data_loader = Amils2023DataLoader()
-    >>> microbes_df = get_microbial_data(data_loader.data_dir)
-
-    """
-
-    microbes_df = pd.read_excel(
-        os.path.join(
-            data_dir,
-            "emi16291-sup-0001-supinfo-tables8-2.ods"
-        ),
-        sheet_name="Sheet1"
-    )
-
-    # Rename pathway column
-    microbes_df = microbes_df.rename(columns={"Pathway/depth": "Pathway"})
-
-    # Drop last row containing the explanation
-    microbes_df = microbes_df.iloc[:-1, :].copy()
-
-    # Drop rows containing the cycles
-    microbes_df = microbes_df[
-        ~microbes_df["Pathway"].str.endswith(" cycle")
-    ]
-
-    # Convert to numeric
-    numeric_cols = [
-        col for col in microbes_df.columns
-        if col not in ["Pathway"]
-    ]
-    microbes_df[numeric_cols] = microbes_df[numeric_cols].apply(
-        pd.to_numeric,
-        errors="coerce"
-    )
-
-    return microbes_df
-
-
 def plot_microbial_data(
     microbes_df: pd.DataFrame
 ) -> plotly.graph_objects.Figure:
@@ -157,7 +103,7 @@ def plot_microbial_data(
     --------
     >>> from biofoundry.data.amils2023 import Amils2023DataLoader
     >>> data_loader = Amils2023DataLoader()
-    >>> microbes_df = get_microbial_data(data_loader.data_dir)
+    >>> microbes_df = data_loader.get_microbial_data()
     >>> plot_microbial_data(microbes_df)
 
     """
