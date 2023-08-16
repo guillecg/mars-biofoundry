06a56eb7f14cd2860feecad2d644c6b0655ef270
guillecg <guillemcg92@gmail.com>
2023-08-16 17:55:27 +0200

    Add logs to medium.py

--- biofoundry/micom/medium.py
+++ biofoundry/micom/medium.py
@@ -1,3 +1,5 @@
+import logging
+
 import os
 import string
 
@@ -6,6 +8,18 @@ import pandas as pd
 from micom.media import Community, minimal_medium
 
 from biofoundry.base import BaseMICOMMediumManager
+
+
+# Configure logging
+logging.basicConfig(
+    filename=os.path.basename(__file__).replace(".py", ".log"),
+    filemode="w",
+    format="%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s - " + \
+        "%(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
 
 
 class MICOMMediumManager(BaseMICOMMediumManager):
@@ -55,10 +69,14 @@ class MICOMMediumManager(BaseMICOMMediumManager):
 
         """
 
+        LOGGER.info("Starting to solve the cooperative tradeoff...")
+
         sol = com.cooperative_tradeoff()
 
         # Extracellular medium has no growth rate
         rates = sol.members.growth_rate.drop("medium")
+
+        LOGGER.info("Starting to get the minimal medium...")
 
         # Get minimal medium
         min_medium = minimal_medium(
@@ -72,6 +90,10 @@ class MICOMMediumManager(BaseMICOMMediumManager):
             .sort_values()\
             .to_frame("flux")\
             .reset_index(names="reaction")
+
+        LOGGER.debug(
+            f"Number of fluxes in the minimal medium: {len(min_medium)}"
+        )
 
         return min_medium
 
@@ -111,6 +133,9 @@ class MICOMMediumManager(BaseMICOMMediumManager):
 
         """
 
+        LOGGER.info(f"Compound name of shared metabolite: {shared_comp}")
+        LOGGER.info(f"Flux name of shared metabolite: {shared_flux}")
+
         # Get ratio for mapping concentrations to fluxes
         shared_conc = medium_df.loc[
             (medium_df["Species"] == shared_comp), "Concentration (ppm)"
@@ -120,20 +145,28 @@ class MICOMMediumManager(BaseMICOMMediumManager):
             (min_medium["reaction"] == shared_flux), "flux"
         ].values[0]
 
+        LOGGER.debug(f"Concentration of shared metabolite: {shared_conc}")
+        LOGGER.debug(f"Flux of shared metabolite: {shared_conc_flux}")
+        LOGGER.info(
+            "Ratio of flux to concentration: " + \
+            str({shared_conc_flux / shared_conc})
+        )
+
         medium_df["flux"] = \
             medium_df["Concentration (ppm)"] * shared_conc_flux / shared_conc
 
-
         # -------------------------------------------------------------------- #
 
-
+        modelseed_path = os.path.join(
+            self.config["paths"]["modelseed"],
+            "compounds.tsv"
+        )
         modelseed_cpd = pd.read_table(
-            os.path.join(
-                self.config["paths"]["modelseed"],
-                "compounds.tsv"
-            ),
+            modelseed_path,
             dtype=object # Avoid warnings
         )
+
+        LOGGER.debug(f"Loaded ModelSEED from {modelseed_path}")
 
         # Try to get the maximum of chemical species
         modelseed_map = modelseed_cpd[
@@ -141,9 +174,16 @@ class MICOMMediumManager(BaseMICOMMediumManager):
             (modelseed_cpd["formula"].isin(medium_df["Species"])) |
             (modelseed_cpd["abbreviation"].isin(medium_df["Species"]))
         ]
+
+        # Get only primary databases sources
         modelseed_map = modelseed_map[
             modelseed_map["source"] == "Primary Database"
         ].copy()
+
+        LOGGER.warning(
+            f"Number of retrieved chemical species from ModelSEED: " + \
+            f"{len(modelseed_map)}/{len(medium_df['Species'])}"
+        )
 
         # Avoid errors in COBRApy due to punctuation and whitespaces in names
         modelseed_map["abbreviation"] = modelseed_map["abbreviation"]\
@@ -177,6 +217,9 @@ class MICOMMediumManager(BaseMICOMMediumManager):
         modelseed_map = modelseed_map[["abbreviation_id", "Species"]]\
             .drop_duplicates()
 
+        LOGGER.debug(f"Dropped duplicates in mapping: {len(modelseed_map)}")
+
+        # Add ModelSEED mapping for matching fluxes in the models 
         medium_mapped = pd.merge(
             left=medium_df,
             right=modelseed_map,
@@ -191,11 +234,19 @@ class MICOMMediumManager(BaseMICOMMediumManager):
         })
         medium_mapped = medium_mapped[["reaction", "flux"]]
 
+        # Add extrapolated fluxes to minimal media
         medium_adjusted = pd.concat(
             [medium_mapped, min_medium],
             axis=0,
             ignore_index=True
-        ).drop_duplicates(keep="first")
+        )
+
+        # Prioritize values for mapped fluxes instead of original ones
+        medium_adjusted = medium_adjusted.drop_duplicates(keep="first")
+
+        LOGGER.info(
+            f"Number of fluxes in the adjusted medium: {len(medium_adjusted)}"
+        )
 
         medium_adjusted = dict(zip(
             medium_adjusted["reaction"],
