9b1676aeb53917fc0c36af446f192931fb4157e4
guillecg <guillemcg92@gmail.com>
2023-08-16 17:21:13 +0200

    Add logs to RetroPath preloader.py

--- biofoundry/retropath/preloader.py
+++ biofoundry/retropath/preloader.py
@@ -1,3 +1,5 @@
+import logging
+
 from typing import Iterable
 
 import os
@@ -9,6 +11,18 @@ import pandas as pd
 from rdkit import Chem
 
 from biofoundry.base import BaseRetroPathPreloader
+
+
+# Configure logging
+logging.basicConfig(
+    filename=os.path.basename(__file__).replace(".py", ".log"),
+    filemode="w",
+    format="%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s - " + \
+        "%(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG
+)
+LOGGER = logging.getLogger(__name__)
 
 
 class RetroPathPreloader(BaseRetroPathPreloader):
@@ -63,20 +77,24 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             for item in model_dict["reactions"]
         ]
 
+        LOGGER.info(f"Number of reaction IDs: {len(reaction_ids)}")
+
         ec_numbers_df = modelseed_reactions[
             modelseed_reactions["id"].isin(reaction_ids)
         ]["ec_numbers"]
 
+        LOGGER.info(f"Number of EC numbers (raw): {len(ec_numbers_df)}")
+
         # Explode series since there may be multiple EC numbers per reaction
         ec_numbers_df = ec_numbers_df.str.split("|").explode()
+
+        LOGGER.info(f"Number of EC numbers: {len(ec_numbers_df)}")
 
         # Convert to frame
         ec_numbers_df = ec_numbers_df.to_frame()
 
         # Add model ID
         ec_numbers_df["ID"] = model_dict["id"]
-
-        # TODO: log statistics
 
         return ec_numbers_df
 
@@ -100,18 +118,24 @@ class RetroPathPreloader(BaseRetroPathPreloader):
 
         """
 
-        # Get EC numbers from reactions in ModelSEEDDatabase
-        modelseed_reactions = pd.read_table(
-            os.path.join(
-                self.config["paths"]["modelseed"],
-                "reactions.tsv"
-            )
+        # Get EC numbers from reactions in ModelSEED
+        modelseed_path = os.path.join(
+            self.config["paths"]["modelseed"],
+            "reactions.tsv"
+        )
+        modelseed_reactions = pd.read_table(modelseed_path)
+
+        LOGGER.debug(f"Loaded ModelSEED from {modelseed_path}")
+        LOGGER.debug(
+             f"Number of ModelSEED reactions: {len(modelseed_reactions)}"
         )
 
         # Create empty dataframe
         all_ec_numbers_df = pd.DataFrame()
 
         for organism in metadata["Code"]:
+
+            LOGGER.info(f"Starting with organism {organism}")
 
             model_path = os.path.join(
                 self.config["paths"]["models"],
@@ -136,11 +160,12 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             )
 
         # Save to file
+        ec_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["ec_numbers"]
+        )
         all_ec_numbers_df.to_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["ec_numbers"]
-            ),
+            ec_path,
             header=True,
             index=False,
             sep=",",
@@ -148,6 +173,9 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             quotechar='"',
             quoting=csv.QUOTE_MINIMAL # Avoid errors with commas in names
         )
+
+        LOGGER.info(f"Saved community EC numbers to {ec_path}")
+        LOGGER.info(f"Total amount of EC numbers: {len(all_ec_numbers_df)}")
 
         return all_ec_numbers_df
 
@@ -172,22 +200,26 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         """
 
         # Load EC numbers in the community
-        ec_num_df = pd.read_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["ec_numbers"]
-            ),
-            sep=","
+        ec_numbers_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["ec_numbers"]
         )
+        ec_num_df = pd.read_csv(ec_numbers_path, sep=",")
+
+        LOGGER.debug(f"Loaded community EC numbers from {ec_numbers_path}")
+        LOGGER.debug(f"Number of ECs: {len(ec_num_df)}")
 
         # Load RetroRules database
-        retrorules_df = pd.read_csv(
-            self.config["paths"]["retrorules"],
-            sep=","
-        )
+        retrorules_path = self.config["paths"]["retrorules"]
+        retrorules_df = pd.read_csv(retrorules_path, sep=",")
+
+        LOGGER.debug(f"Loaded RetroRules from {retrorules_path}")
+        LOGGER.debug(f"Number of RetroRules: {len(retrorules_df)}")
 
         # Drop potential duplicates (more than one species with the same EC)
         ec_numbers = ec_num_df["ec_numbers"].unique()
+
+        LOGGER.debug(f"Dropped duplicates in ECs: {len(ec_numbers)} unique")
 
         # Explode rules with multiple EC numbers
         retrorules_df["EC number"] = retrorules_df["EC number"].str.split(";")
@@ -195,23 +227,32 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             .explode("EC number")\
             .reset_index(drop=True)
 
+        LOGGER.debug(f"Exploded ECs in RetroRules: {len(retrorules_df)} total")
+
         # Filter by found EC numbers in the community
         rules_df = retrorules_df[
             retrorules_df["EC number"].isin(ec_numbers)
         ]
 
+        LOGGER.info(
+            f"Retrieved rules by ECs present in the community: {len(rules_df)}"
+        )
+
         # Save to file
+        rules_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["rules"]
+        )
         rules_df.to_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["rules"]
-            ),
+            rules_path,
             header=True,
             index=False,
             sep=",",
             quotechar='"',
             quoting=csv.QUOTE_MINIMAL # Avoid errors with commas in names
         )
+
+        LOGGER.info(f"Saved community rules to {rules_path}")
 
         return rules_df
 
@@ -237,11 +278,12 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         """
 
         # Read MetaNetX reac_prop.tsv file
+        metanetx_reac_path = os.path.join(
+            self.config["paths"]["metanetx"],
+            "reac_prop.tsv"
+        )
         metanetx_reac_prop = pd.read_table(
-            os.path.join(
-                self.config["paths"]["metanetx"],
-                "reac_prop.tsv"
-            ),
+            metanetx_reac_path,
             comment="#", # Skip comment rows
             header=None,
             names=[
@@ -254,12 +296,16 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             ]
         )
 
+        LOGGER.debug(f"Loaded MetaNetX reactions from {metanetx_reac_path}")
+        LOGGER.debug(f"Number of MetaNetX reactions: {len(metanetx_reac_prop)}")
+
         # Read MetaNetX chem_prop.tsv file
+        metanetx_chem_path = os.path.join(
+            self.config["paths"]["metanetx"],
+            "chem_prop.tsv"
+        )
         metanetx_chem_prop = pd.read_table(
-            os.path.join(
-                self.config["paths"]["metanetx"],
-                "chem_prop.tsv"
-            ),
+            metanetx_chem_path,
             comment="#", # Skip comment rows
             header=None,
             names=[
@@ -275,14 +321,18 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             ]
         )
 
+        LOGGER.debug(f"Loaded MetaNetX compounds from {metanetx_chem_path}")
+        LOGGER.debug(f"Number of MetaNetX compounds: {len(metanetx_chem_prop)}")
+
         # Load rules extracted by mapping ECs to RetroRules DB
-        rules_df = pd.read_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["rules"]
-            ),
-            sep=","
+        rules_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["rules"]
         )
+        rules_df = pd.read_csv(rules_path, sep=",")
+
+        LOGGER.debug(f"Loaded community rules from {rules_path}")
+        LOGGER.debug(f"Number of community rules: {len(rules_df)}")
 
         # Get all MetaNetX reaction IDs
         rules_df[["MNXR", "MNXM"]]  = rules_df["Rule ID"]\
@@ -291,6 +341,11 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         # Drop diameter and rule usage duplicates
         unique_rules_df = rules_df\
             .drop_duplicates(subset=["MNXR", "Rule usage"])
+
+        LOGGER.debug(
+            "Dropped duplicated rules (diameter and usage columns): " + \
+            str(len(unique_rules_df))
+        )
 
         # Get all reaction information by MNXR
         reactions_df = pd.merge(
@@ -302,14 +357,17 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             indicator=True
         )
 
-        # Remove "left_only" reactions (not present in the organism rules)
-        reactions_df = reactions_df[reactions_df["_merge"] != "left_only"]
-
-        # Get all reactions that could not be mapped in METANETX
-        not_in_metanetx = reactions_df[reactions_df["_merge"] == "right_only"]
-        print(
-            "[WARNING] #reactions not present in METANETX: ",
-            len(not_in_metanetx), "/", len(reactions_df)
+        LOGGER.debug(
+            "Number of reactions only present in MetaNetX: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "right_only"]))
+        )
+        LOGGER.debug(
+            "Number of reactions only present in the community: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "left_only"]))
+        )
+        LOGGER.debug(
+            "Number of reactions shared: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "both"]))
         )
 
         # Finally, get reactions that could be mapped to METANETX
@@ -323,6 +381,11 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             .reset_index(drop=True)\
             .rename(columns={0: "Name"})
 
+        LOGGER.debug(
+            "Number of compounds in forward rules: " + \
+            str(len(compounds_forward))
+        )
+
         # Add products of reversible reactions
         compounds_reverse = reactions_df[
                 reactions_df["Rule usage"] == "both"
@@ -332,6 +395,11 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             .reset_index(drop=True)\
             .rename(columns={0: "Name"})
 
+        LOGGER.debug(
+            "Number of compounds in reversible rules: " + \
+            str(len(compounds_reverse))
+        )
+
         # Merge substrates and products (reversible reactions)
         sink_df = pd.concat(
             [compounds_forward, compounds_reverse],
@@ -339,8 +407,12 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             ignore_index=True
         )
 
+        LOGGER.debug(f"Number of compounds in sink (raw): {len(sink_df)}")
+
         # Drop potential duplicates (compounds in more than one reation)
         sink_df = sink_df.drop_duplicates()
+
+        LOGGER.debug(f"Dropped duplicates in sink (raw): {len(sink_df)}")
 
         # Get InChIs for the extracted compound IDs
         sink_df = pd.merge(
@@ -351,22 +423,22 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         )
         sink_df = sink_df[["Name", "InChI"]]
 
-        # WARNING: there are compounds without InChI
-        no_inchi = sink_df[sink_df["InChI"].isnull()]
-        print(
-            "[WARNING] #compounds without InChI: ",
-            len(no_inchi), "/", len(sink_df)
+        LOGGER.warning(
+            "Number of compounds without InChI: " + \
+            str(len(sink_df[sink_df["InChI"].isnull()])) + "/" + \
+            str(len(sink_df))
         )
 
         # Fill missing InChIs and match sink.csv format
         sink_df = sink_df.fillna("None")
 
         # Save to file
+        sink_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["sink"]
+        )
         sink_df.to_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["sink"]
-            ),
+            sink_path,
             header=True,
             index=False,
             sep=",",
@@ -374,6 +446,8 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             quotechar='"',
             quoting=csv.QUOTE_ALL # Fit sink.csv format for RetroPath2.0
         )
+
+        LOGGER.info(f"Saved community sink to {sink_path}")
 
         return sink_df
 
@@ -398,13 +472,14 @@ class RetroPathPreloader(BaseRetroPathPreloader):
 
         """
 
-        sources_df = pd.read_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                "interesting_metabolites/",
-                self.config["retropath"]["files"]["sources"]
-            )
+        sources_path = os.path.join(
+            self.config["paths"]["retropath"],
+            "interesting_metabolites/",
+            self.config["retropath"]["files"]["sources"]
         )
+        sources_df = pd.read_csv(sources_path)
+
+        LOGGER.debug(f"Loaded sources from {sources_path}")
 
         # Get InChIs
         sources_df["InChI"] = \
@@ -417,7 +492,11 @@ class RetroPathPreloader(BaseRetroPathPreloader):
                 )
             )
 
-        # TODO: log compounds with missing InChI
+        LOGGER.warning(
+            "Number of compounds without InChI: " + \
+            str(len(sources_df[sources_df["InChI"].isnull()])) + "/" + \
+            str(len(sources_df))
+        )
 
         # Drop metabolites withou InChI
         sources_df = sources_df.dropna(subset="InChI")
@@ -436,16 +515,19 @@ class RetroPathPreloader(BaseRetroPathPreloader):
 
             row = row.to_frame().T[["Name", "InChI"]]
 
+            compound_path = os.path.join(
+                self.config["paths"]["retropath"],
+                "interesting_metabolites/",
+                "sources/",
+                f"{compound_name}.csv"
+            )
             row.to_csv(
-                os.path.join(
-                    self.config["paths"]["retropath"],
-                    "interesting_metabolites/",
-                    "sources/",
-                    f"{compound_name}.csv"
-                ),
+                compound_path,
                 header=True,
                 index=False
             )
+
+            LOGGER.info(f"Saved compound to {compound_path}")
 
         return sources_df
 
@@ -472,30 +554,40 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         None
 
         """
-        
+
         source_str = r"|".join(source_ids)
 
         # Load EC numbers and rules in the community
-        ec_num_df = pd.read_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                self.config["retropath"]["files"]["ec_numbers"]
-            ),
-            sep=","
+        ec_numbers_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["ec_numbers"]
         )
-        rules_df = pd.read_csv(
-            os.path.join(
-                self.config["paths"]["retropath"],
-                "rules.csv"
-            )
+        ec_num_df = pd.read_csv(ec_numbers_path, sep=",")
+
+        LOGGER.debug(f"Loaded community EC numbers from {ec_numbers_path}")
+        LOGGER.debug(f"Number of ECs: {len(ec_num_df)}")
+
+        rules_path = os.path.join(
+            self.config["paths"]["retropath"],
+            self.config["retropath"]["files"]["rules"]
         )
+        rules_df = pd.read_csv(rules_path, sep=",")
 
-        # Drop potential EC duplicates (more than one species with the same EC)
-        ec_num_df = ec_num_df.drop_duplicates()
+        LOGGER.debug(f"Loaded community rules from {rules_path}")
+        LOGGER.debug(f"Number of community rules: {len(rules_df)}")
 
-        # Avoid potential duplicates because of different rule usages and 
-        # diameters
+        # Drop potential duplicates (more than one species with the same EC)
+        ec_numbers = ec_num_df["ec_numbers"].unique()
+
+        LOGGER.debug(f"Dropped duplicates in ECs: {len(ec_numbers)} unique")
+
+        # Avoid duplicates because of different rule usages and diameters
         rules_df = rules_df.drop(["Diameter", "Rule usage"], axis=1)
+
+        LOGGER.debug(
+            "Dropped duplicated rules (diameter and usage columns): " + \
+            str(len(rules_df))
+        )
 
         # Add organism to rules
         rules_df = pd.merge(
@@ -508,11 +600,12 @@ class RetroPathPreloader(BaseRetroPathPreloader):
         rules_df = rules_df.rename(columns={"ID": "Organism"})
 
         # Read MetaNetX reac_prop.tsv file
+        metanetx_reac_path = os.path.join(
+            self.config["paths"]["metanetx"],
+            "reac_prop.tsv"
+        )
         metanetx_reac_prop = pd.read_table(
-            os.path.join(
-                self.config["paths"]["metanetx"],
-                "reac_prop.tsv"
-            ),
+            metanetx_reac_path,
             comment="#", # Skip comment rows
             header=None,
             names=[
@@ -524,6 +617,9 @@ class RetroPathPreloader(BaseRetroPathPreloader):
                 "is_transport"
             ]
         )
+
+        LOGGER.debug(f"Loaded MetaNetX reactions from {metanetx_reac_path}")
+        LOGGER.debug(f"Number of MetaNetX reactions: {len(metanetx_reac_prop)}")
 
         # Get all MetaNetX reaction IDs
         rules_df[["MNXR", "MNXM"]]  = rules_df["Rule ID"]\
@@ -539,17 +635,20 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             indicator=True
         )
 
-        # Remove "left_only" reactions (not present in the organism rules)
-        reactions_df = reactions_df[reactions_df["_merge"] != "left_only"]
-
-        # Get all reactions that could not be mapped in METANETX
-        not_in_metanetx = reactions_df[reactions_df["_merge"] == "right_only"]
-        print(
-            "[WARNING] #reactions not present in METANETX: ",
-            len(not_in_metanetx), "/", len(reactions_df)
+        LOGGER.debug(
+            "Number of reactions only present in MetaNetX: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "right_only"]))
+        )
+        LOGGER.debug(
+            "Number of reactions only present in the community: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "left_only"]))
+        )
+        LOGGER.debug(
+            "Number of reactions shared: " + \
+            str(len(reactions_df[reactions_df["_merge"] == "both"]))
         )
 
-        # Finally, get reactions in the organism that could be mapped to METANETX
+        # Finally, get reactions that could be mapped to METANETX
         reactions_df = reactions_df[reactions_df["_merge"] == "both"]
         reactions_df["Source match"] = reactions_df["mnx_equation"]\
             .str.findall(source_str)\
@@ -560,8 +659,9 @@ class RetroPathPreloader(BaseRetroPathPreloader):
             reactions_df["Source match"] != ""
         ]
 
-        # TODO: check why duplicates appear
         reactions_df = reactions_df[["Organism", "Source match"]]\
             .drop_duplicates()
+
+        LOGGER.debug(f"Dropped duplicates in matches: {len(reactions_df)}")
 
         return reactions_df
