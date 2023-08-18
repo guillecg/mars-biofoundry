0e06bd5b94de9dec6f34c9239ee8aa5cd2951259
guillecg <guillemcg92@gmail.com>
2023-08-18 21:10:51 +0200

    Change test config filename

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import yaml
 
 
 @pytest.fixture(scope="session")
-def config(config_path: str = "tests/config.yml") -> dict:
+def config(config_path: str = "tests/config-test.yml") -> dict:
 
     with open(config_path) as config_file:
         config = yaml.safe_load(config_file)
