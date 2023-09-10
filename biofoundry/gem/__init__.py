9d5137daced3991d75c5cadc71e1ca78ffd0b13b
guillecg <guillemcg92@gmail.com>
2023-09-10 17:13:40 +0200

    Add ModelValidator in gem __init__.py

--- biofoundry/gem/__init__.py
+++ biofoundry/gem/__init__.py
@@ -1,9 +1,10 @@
-from .model import ModelBuilder
+from .model import ModelBuilder, ModelValidator
 from .plots import plot_metabolic_models
 from .utils import get_gene_counts
 
 __all__ = [
     "ModelBuilder",
+    "ModelValidator",
     "plot_metabolic_models",
     "get_gene_counts"
 ]
