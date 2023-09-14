a52f6463719b6c629dfbf3305aa62c3359acc848
guillecg <guillemcg92@gmail.com>
2023-09-14 13:24:02 +0200

    Add metadata_df fixture

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 import yaml
 
+import pandas as pd
+
 
 @pytest.fixture(scope="session")
 def config(config_path: str = "tests/config-test.yml") -> dict:
@@ -10,3 +12,18 @@ def config(config_path: str = "tests/config-test.yml") -> dict:
         config = yaml.safe_load(config_file)
 
     return config
+
+
+@pytest.fixture(scope="session")
+def metadata_df() -> pd.DataFrame:
+    return pd.DataFrame.from_records([
+        {
+            "Species": "NA",
+            "Code": "test_model",
+            "Database": "NA",
+            "ID": "NA",
+            "Record": "NA",
+            "Download": "NA",
+            "Protein annotation file": "NA"
+        }
+    ])
